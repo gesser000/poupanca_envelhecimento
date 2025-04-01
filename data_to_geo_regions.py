e3013fa13e006cdde14e5ffca3c9178d4c3403b6
gesser000 <gesser000@gmail.com>
2025-04-01 13:36:33 -0300

    update

--- data_to_geo_regions.py
+++ data_to_geo_regions.py
@@ -8,6 +8,9 @@ dicionario = pd.read_csv("dicionarios/dicionario_geografia.csv", encoding = "Lat
 censo.rename(columns={"ano": "dt_year", "UF": "uf", "codmun": "codmun_ibge"}, inplace=True)
 censo["dt_year"] = censo["dt_year"].astype(float)
 censo["codmun_ibge"] = censo["codmun_ibge"].astype(float)
+censo["idosos"] = censo["pop_expandida"] * censo["tx_65_anos"]
+censo["pop_ativa"] = censo["pop_expandida"] * censo["tx_pop_ativa"]
+censo["razao_dependencia_idosos"] = censo["idosos"] / censo["pop_ativa"]
 
 estban = estban[['dt_year', 'CODMUN_IBGE', 'AGEN_PROCESSADAS', 'VERBETE_420_DEPOSITOS_DE_POUPANCA', 'VERBETE_432_DEPOSITOS_A_PRAZO']].copy()
 estban.rename(columns={"CODMUN_IBGE": "codmun_ibge", "AGEN_PROCESSADAS": "agen_processadas", "VERBETE_420_DEPOSITOS_DE_POUPANCA": "depositos_poupanca", "VERBETE_432_DEPOSITOS_A_PRAZO": "depositos_prazo"}, inplace=True)
@@ -19,9 +22,9 @@ df_merged = pd.merge(df_merged, dicionario, on="codmun_ibge", how="left")
 
 columns = ['sexo', 'idade', 'raca', 'branco',
        'anos_mor_mun', 'nasceu_mun', 'anos_estudoC', 'filhos_tot',
-       'filhos_nasc_vivos', 'vive_conjuge', 'estado_conj', '65_anos', 'casado', 'sit_setor_C', 'cond_ocup_B', 'ocup_propria',
+       'filhos_nasc_vivos', 'vive_conjuge', 'estado_conj', 'tx_casado', 'tx_65_anos', 'tx_pop_ativa', 'tx_14_anos', 'sit_setor_C', 'razao_dependencia_idosos', 'cond_ocup_B', 'ocup_propria',
        'especie', 'ilum_eletr', 'lavaroupa', 'n_pes_dom', 'sanitario',
-       'renda_dom']
+       ]
 
 df_reg_imediata = df_merged.groupby(['dt_year', 'id_regiao',
        'nm_regiao', 'id_uf', 'nm_uf',
@@ -33,6 +36,7 @@ df_reg_imediata = df_merged.groupby(['dt_year', 'id_regiao',
            depositos_poupanca=("depositos_poupanca", "sum"),
            depositos_prazo=("depositos_prazo", "sum"),
            depositos_totais=("depositos_totais", "sum"),
+           renda_dom=("renda_dom", "sum"),
            area_km2=("area_km2", "sum"),
            deflator=("deflator", "first"),
            conversor=("conversor", "first"),
@@ -50,9 +54,9 @@ df_reg_imediata["depositos_prazo_deflacionada_per_capita"] = df_reg_imediata["de
 df_reg_imediata["depositos_totais_deflacionada_per_capita"] = df_reg_imediata["depositos_totais_deflacionada"] / df_reg_imediata["pop_expandida_micro"]
 
 df_reg_imediata["log_renda_dom_deflacionada_per_capita"] = np.log(df_reg_imediata["renda_dom_deflacionada_per_capita"])
-df_reg_imediata["log_depositos_poupanca_deflacionada_per_capita"] = np.log(df_reg_imediata["depositos_poupanca_deflacionada_per_capita"])
-df_reg_imediata["log_depositos_prazo_deflacionada_per_capita"] = np.log(df_reg_imediata["depositos_prazo_deflacionada_per_capita"])
-df_reg_imediata["log_depositos_totais_deflacionada_per_capita"] = np.log(df_reg_imediata["depositos_totais_deflacionada_per_capita"])
+df_reg_imediata["log_depositos_poupanca_deflacionada_per_capita"] = np.log(df_reg_imediata["depositos_poupanca_deflacionada_per_capita"] + 1)
+df_reg_imediata["log_depositos_prazo_deflacionada_per_capita"] = np.log(df_reg_imediata["depositos_prazo_deflacionada_per_capita"] + 1)
+df_reg_imediata["log_depositos_totais_deflacionada_per_capita"] = np.log(df_reg_imediata["depositos_totais_deflacionada_per_capita"] + 1)
 
 df_reg_imediata.to_parquet("dados_agrupados/censo_agrupado_por_regioes_imediatas.gzip", compression="gzip")
 df_reg_imediata.to_excel("dados_agrupados/censo_agrupado_por_regioes_imediatas.xlsx")
--- microdata_refinement.py
+++ microdata_refinement.py
@@ -13,9 +13,10 @@ class MDR:
         self.DOM_COLUMNS = [
             "ano",
             "UF",
-            "munic",
+            "codmun",
             "sit_setor_C",
             "especie",
+            "cond_ocup_B",
             "ilum_eletr",
             "lavaroupa",
             "n_pes_dom",
@@ -29,10 +30,11 @@ class MDR:
         self.PES_COLUMNS = [
             "ano",
             "UF",
-            "munic",
+            "codmun",
             "sexo",
             "idade",
             "raca",
+            "nasceu_mun",
             "anos_mor_mun",
             "anos_estudoC",
             "filhos_tot",
@@ -60,27 +62,32 @@ class MDR:
             pes = self.load_microdata(year, "pes", self.PES_COLUMNS)
             dom = self.load_microdata(year, "dom", self.DOM_COLUMNS)
 
-            pes["65_anos"] = (pes["idade"] >= 65).astype(int)
-            pes["casado"] = (pes["estado_conj"].isin([1, 2, 3, 4])).astype(int)
+            pes["tx_65_anos"] = (pes["idade"] >= 65).astype(int)
+            pes["tx_14_anos"] = (pes["idade"] <= 14).astype(int)
+            pes["tx_pop_ativa"] = ((pes["idade"] >= 14) & (pes["idade"] <= 65)).astype(int)
+            pes["tx_casado"] = (pes["estado_conj"].isin([1, 2, 3, 4])).astype(int)
+            pes["branco"] = (pes["raca"] == 1).astype(int)
+            dom["ocup_propria"] = (dom["cond_ocup_B"] == 1).astype(int)
 
-            pes_vars = ["sexo", "idade", "raca", "anos_mor_mun", "anos_estudoC", "filhos_tot", "filhos_nasc_vivos", "vive_conjuge", "estado_conj", "65_anos", "casado"]
-            dom_vars = ["sit_setor_C", "especie", "ilum_eletr", "lavaroupa", "n_pes_dom", "sanitario", "renda_dom"]
+            pes_vars = ["sexo", "idade", "raca", "branco", "anos_mor_mun", "nasceu_mun", "anos_estudoC", "filhos_tot", "filhos_nasc_vivos", "vive_conjuge", "estado_conj", "tx_65_anos", "tx_14_anos", "tx_pop_ativa", "tx_casado"]
+            dom_vars = ["sit_setor_C", "cond_ocup_B", "ocup_propria", "especie", "ilum_eletr", "lavaroupa", "n_pes_dom", "sanitario"]
 
             # Agrupamento para tabela de pessoas
-            pes_mun = pes.groupby(["ano", "UF", "munic"], as_index=False).agg(
+            pes_mun = pes.groupby(["ano", "UF", "codmun"], as_index=False).agg(
             pop_expandida=("peso_pess", "sum"),
             individuos_amostrados=("peso_pess", "count"),
             **{var: (var, lambda g: (g * pes.loc[g.index, "peso_pess"]).sum() / pes.loc[g.index, "peso_pess"].sum()) for var in pes_vars}
             )
 
             # Agrupamento para tabela de domicílios
-            dom_mun = dom.groupby(["ano", "UF", "munic"], as_index=False).agg(
+            dom_mun = dom.groupby(["ano", "UF", "codmun"], as_index=False).agg(
+            renda_dom=("renda_dom", "sum"),
             deflator=("deflator", "first"),
             conversor=("conversor", "first"),
             **{var: (var, lambda g: (g * dom.loc[g.index, "peso_dom"]).sum() / dom.loc[g.index, "peso_dom"].sum()) for var in dom_vars}
             )
 
-            censo = pd.merge(pes_mun, dom_mun, on=["ano", "UF", "munic"])
+            censo = pd.merge(pes_mun, dom_mun, on=["ano", "UF", "codmun"])
 
             assert len(pes_mun) == len(dom_mun) == len(censo), f"Error: Difference in the number of municipalities in the year {year}"
 
